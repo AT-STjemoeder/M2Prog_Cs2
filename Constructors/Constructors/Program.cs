d301f3ab00679d099045cdec6556f10bad5910dc
lucas <37939@ma-web.nl>
2024-12-02 12:07:07 +0100

    hey

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+            private void run()
+            {
+            QuizVraag quizVraag = new QuizVraag("hier komt de vraag", "hier komt het antwoord");
+        
+            }
+    }  
+    
+    
+    class QuizVraag
+                {
+                    internal string vraag;
+                    internal string antwoord;
+
+                    internal QuizVraag(string vraag, string antwoord)
+                    {
+                        this.vraag = vraag;
+                        this.antwoord = antwoord;
+                    }
+                } 
+    class Quiz
+    {
+        internal QuizVraag[] vragen;
+        internal QuizVraagAntwoord[] ingevuldeAntwoorden;
+    }
+    class QuizVraagAntwoord
+    {
+        internal QuizVraag vraag;
+        internal bool goed;
+    }
+}

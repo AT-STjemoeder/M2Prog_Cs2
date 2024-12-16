c526867a79febc290218e1763954f3cbb140393c
lucas <37939@ma-web.nl>
2024-12-16 11:24:27 +0100

    c#

--- Constructors/Constructors/Program.cs
+++ Constructors/Constructors/Program.cs
@@ -11,7 +11,8 @@ namespace Constructors
             private void run()
             {
             QuizVraag quizVraag = new QuizVraag("hier komt de vraag", "hier komt het antwoord");
-        
+            Quiz quiz = new Quiz(10);
+
             }
     }  
     
@@ -29,12 +30,26 @@ namespace Constructors
                 } 
     class Quiz
     {
+        internal void VoegVraagToeOpIndex(int index, QuizVraag vraag)
+        {
+            vragen[index] = vraag;
+        }
+        internal Quiz(int aantalVragen) 
+        {
+          vragen = new QuizVraag[aantalVragen];
+        }
         internal QuizVraag[] vragen;
         internal QuizVraagAntwoord[] ingevuldeAntwoorden;
     }
     class QuizVraagAntwoord
     {
+        internal QuizVraagAntwoord(QuizVraag vraag) 
+        {
+            this .vraag = vraag;
+            goed = false;
+        }
         internal QuizVraag vraag;
         internal bool goed;
     }
+    
 }
